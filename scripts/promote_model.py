a741a037d8b547640f75dc2f34bbac159c99f47f
nikhil <nihkilmogre1998@gmail.com>
2025-04-04 20:19:34 +0530

    added model laoding test

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -3,7 +3,7 @@ import mlflow
 
 def promote_model():
     # Set up AWS MLflow tracking URI
-    mlflow.set_tracking_uri("http://ec2-54-196-109-131.compute-1.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://ec2-3-106-222-228.ap-southeast-2.compute.amazonaws.com:5000/")
 
     client = mlflow.MlflowClient()
 
